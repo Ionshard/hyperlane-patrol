78ccf9ba256d6bc7473b8784faa50cd415933ed6
Victor Ling <victorling@ionshard.com>
2024-05-10 20:59:06 -0400

    Fix cursor issue

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -52,8 +52,6 @@ export class Game extends Scene {
     const embeddedUrl = `url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAFBhaW50Lk5FVCB2My41LjbQg61aAAAADUlEQVQYV2P4//8/IwAI/QL/+TZZdwAAAABJRU5ErkJggg==')`;
     const hostedUrl = `url(assets/blank.cur)`;
     const fallback = `none`;
-    this.input.setDefaultCursor(
-      `${embeddedUrl}, ${hostedUrl}, ${fallback} !important`
-    );
+    this.input.setDefaultCursor(`${embeddedUrl}, ${hostedUrl}, ${fallback}`);
   }
 }
